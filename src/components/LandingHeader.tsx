773dad397678fa3c2a93678be18b710b60a0f12c
6TG9 <andrewmjr2@gmail.com>
2026-04-17 13:19:49 -0700

    first commit

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -4,9 +4,13 @@ export function LandingHeader() {
   return (
     <header className="bg-white border-b px-4 sm:px-6 py-4 flex justify-between items-center">
       <div>
-        <img src="/MERCED.jpg" className="h-8 sm:h-12" alt="" />
+        <img
+          src="/logo.png"
+          className="w-[120px] sm:w-[150px]"
+          alt="Aptia365 Logo"
+        />
       </div>
-      <div>
+      <div className="hidden">
         <img
           src="/logo.png"
           className="w-[120px] sm:w-[150px]"
